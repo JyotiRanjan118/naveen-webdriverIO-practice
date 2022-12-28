83774d1a03502210a1560e1a25868e1204137fc4
Jyoti Nath <jyoti.nath@publicissapient.com>
2022-12-28 18:22:19 +0530

    Web table part_02 done

--- test/features/step-definitions/demo.ts
+++ test/features/step-definitions/demo.ts
@@ -31,6 +31,17 @@ Then(/^URL should match (.*)$/, async function (expectedURL) {
  * Web Interactions
  *
  */
+
+/**
+ * Web Table
+ * What are going to cover
+ * 1. Check number of rows and columns
+ * 2. Get whole table data
+ * 3. Get single row based on a condition
+ * 4. Get single column
+ * 5. Get single cell value based on another cell
+ *
+ */
 Given(/^A web page is opened$/, async function () {
   await browser.url("https://the-internet.herokuapp.com/tables");
   await browser.setTimeout({ implicit: 15000, pageLoad: 10000 });
@@ -44,46 +55,102 @@ Given(/^A web page is opened$/, async function () {
 
   /**
    * Get whole table data
+   * Check number of rows and columns
+   * Get whole table data
+   */
+  // let arr = [];
+  // for (let i = 0; i < rowCount; i++) {
+  //   let personJob = {
+  //     lastname: "",
+  //     firstname: "",
+  //     email: "",
+  //     due: "",
+  //     web: "",
+  //   };
+  //   for (let j = 0; j < colCount; j++) {
+  //     let cellVal = await $(
+  //       `//table[@id='table1']/tbody/tr[${i + 1}]/td[${j + 1}]`
+  //     ).getText();
+  //     /**
+  //      * Assign the values to object
+  //      */
+  //     if (cellVal === "") {
+  //       if (j === 0) personJob.lastname = cellVal;
+  //       if (j === 1) personJob.firstname = cellVal;
+  //       if (j === 2) personJob.email = cellVal;
+  //       if (j === 3) personJob.due = cellVal;
+  //       if (j === 4) personJob.web = cellVal;
+  //     }
+
+  //     //console.log(`>> Cell value: ${cellVal}`);
+  //   }
+  //   arr.push(personJob);
+  // }
+  // console.log(`Whole table: ${JSON.stringify(arr)}`);
+
+  /**
+   * Web table part 2
+   * 3. get a single row based on condition
    *
+   */
+  // let arr = [];
+  // for (let i = 0; i < rowCount; i++) {
+  //   let personJob = {
+  //     lastname: "",
+  //     firstname: "",
+  //     email: "",
+  //     due: "",
+  //     web: "",
+  //   };
+  //   for (let j = 0; j < colCount; j++) {
+  //     let cellVal = await $(
+  //       `//table[@id='table1']/tbody/tr[${i + 1}]/td[${j + 1}]`
+  //     ).getText();
+  //     let firstname = await $(
+  //       `//table[@id='table1']/tbody/tr[${i + 1}]/td[2]`
+  //     ).getText();
+  //     if (firstname === "Jason") {
+  //       if (j === 0) personJob.lastname = cellVal;
+  //       if (j === 1) personJob.firstname = cellVal;
+  //       if (j === 2) personJob.email = cellVal;
+  //       if (j === 3) personJob.due = cellVal;
+  //       if (j === 4) personJob.web = cellVal;
+  //     }
+  //   }
+  //   if (personJob.firstname) {
+  //     arr.push(personJob);
+  //   }
+  // }
+  // console.log(`Whole table: ${JSON.stringify(arr)}`);
+
+  /**
+   * 4.Get single column
+   */
+  // let arr = [];
+  // for (let i = 0; i < rowCount; i++) {
+  //   let cellVal = await $(
+  //     `//table[@id='table1']/tbody/tr[${i + 1}]/td[4]`
+  //   ).getText();
+  //   arr.push(cellVal);
+  // }
+  // console.log(`>> Single col value: ${arr}`);
+
+  /**
+   * 5. Get single cell value based on another cell
    */
   let arr = [];
   for (let i = 0; i < rowCount; i++) {
-    let personJob = {
-      lastname: "",
-      firstname: "",
-      email: "",
-      due: "",
-      web: "",
-    };
-    for (let j = 0; j < colCount; j++) {
-      let cellVal = await $(
-        `//table[@id='table1']/tbody/tr[${i + 1}]/td[${j + 1}]`
-      ).getText();
-      /**
-       * Assign the values to object
-       */
-      if (j === 0) personJob.lastname = cellVal;
-      if (j === 1) personJob.firstname = cellVal;
-      if (j === 2) personJob.email = cellVal;
-      if (j === 3) personJob.due = cellVal;
-      if (j === 4) personJob.web = cellVal;
-
-      //console.log(`>> Cell value: ${cellVal}`);
+    //for (let j = 0; j < colCount; j++) {
+    let price = await (
+      await $(`//table[@id='table1']/tbody/tr[${i + 1}]/td[4]`)
+    ).getText();
+    let firstname = await (
+      await $(`//table[@id='table1']/tbody/tr[${i + 1}]/td[2]`)
+    ).getText();
+    if (+price.replace("$", "") > 50) {
+      arr.push(firstname);
     }
-    arr.push(personJob);
+    //}
   }
-  console.log(`Whole table: ${JSON.stringify(arr)}`);
-
-  //await browser.debug();
+  console.log(`>> Single col value: ${arr}`);
 });
-
-/**
- * Web Table
- * What are going to cover
- * 1. Check number of rows and columns
- * 2. Get whole table data
- * 3. Get single row based on a condition
- * 4. Get single column
- * 5. Get single cell value based on another cell
- *
- */
